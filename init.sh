53a86f442bb69f9da506303f483af9a6b410a0f9
Rasmus Eskola <fruitiex@gmail.com>
2014-06-23 13:00:01 +0300

    add latency/pl both up/down

--- init.sh
+++ init.sh
@@ -11,3 +11,5 @@ ip link set dev ifb0 up
 tc qdisc add dev $INTERFACE ingress
 tc filter add dev $INTERFACE parent ffff: \
 	protocol ip u32 match u32 0 0 flowid 1:1 action mirred egress redirect dev ifb0
+
+tc qdisc add dev ifb0 root netem
--- netem.sh
+++ netem.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 INTERFACE=$1
-LATENCY=$2
-PACKETLOSS=$3
+LATENCY=$(($2/2))
+PACKETLOSS=$(($3/2))
 
 if [[ -z $3 ]]; then
 	echo Usage: netem.sh INAME LATENCY PACKETLOSS
@@ -10,3 +10,4 @@ if [[ -z $3 ]]; then
 fi
 
 sudo tc qdisc change dev "$INTERFACE" root netem delay "${LATENCY}ms" loss "${PACKETLOSS}%"
+sudo tc qdisc change dev "ifb0" root netem delay "${LATENCY}ms" loss "${PACKETLOSS}%"
